8cf37e93ef1eeb417695428f57fd59a28cc4338d
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-05 14:14:33 +0100

    New

---
+++ UZahlenraten/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace UZahlenraten
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnErzeugen = new System.Windows.Forms.Button();
+			this.btnPrüfen = new System.Windows.Forms.Button();
+			this.lblAusgabe = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtEingabe = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnErzeugen
+			// 
+			this.btnErzeugen.Location = new System.Drawing.Point(235, 4);
+			this.btnErzeugen.Name = "btnErzeugen";
+			this.btnErzeugen.Size = new System.Drawing.Size(75, 23);
+			this.btnErzeugen.TabIndex = 0;
+			this.btnErzeugen.Text = "Erzeugen";
+			this.btnErzeugen.UseVisualStyleBackColor = true;
+			this.btnErzeugen.Click += new System.EventHandler(this.btnErzeugen_Click);
+			// 
+			// btnPrüfen
+			// 
+			this.btnPrüfen.Location = new System.Drawing.Point(235, 33);
+			this.btnPrüfen.Name = "btnPrüfen";
+			this.btnPrüfen.Size = new System.Drawing.Size(75, 23);
+			this.btnPrüfen.TabIndex = 1;
+			this.btnPrüfen.Text = "Prüfen";
+			this.btnPrüfen.UseVisualStyleBackColor = true;
+			this.btnPrüfen.Click += new System.EventHandler(this.btnPrüfen_Click);
+			// 
+			// lblAusgabe
+			// 
+			this.lblAusgabe.AutoSize = true;
+			this.lblAusgabe.Location = new System.Drawing.Point(16, 70);
+			this.lblAusgabe.Name = "lblAusgabe";
+			this.lblAusgabe.Size = new System.Drawing.Size(35, 13);
+			this.lblAusgabe.TabIndex = 2;
+			this.lblAusgabe.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(49, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Eingabe:";
+			// 
+			// txtEingabe
+			// 
+			this.txtEingabe.Location = new System.Drawing.Point(108, 35);
+			this.txtEingabe.Name = "txtEingabe";
+			this.txtEingabe.Size = new System.Drawing.Size(100, 20);
+			this.txtEingabe.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(325, 100);
+			this.Controls.Add(this.txtEingabe);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lblAusgabe);
+			this.Controls.Add(this.btnPrüfen);
+			this.Controls.Add(this.btnErzeugen);
+			this.Name = "Form1";
+			this.Text = "Zahl zwischen 1 und 100";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnErzeugen;
+		private System.Windows.Forms.Button btnPrüfen;
+		private System.Windows.Forms.Label lblAusgabe;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtEingabe;
+	}
+}
+
---
+++ UZahlenraten/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UZahlenraten
+{
+	public partial class Form1 : Form
+	{
+		// Zufallszahlengenerator initialisieren, vgl. Buch S. 100
+		private readonly Random r = new Random();
+
+		// Globale Variable, in der die Zufallszahl gespeichert wird
+		private int zufallszahl = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnErzeugen_Click(object sender, EventArgs e)
+		{
+			// Neue Zufallszahl erzeugen
+			zufallszahl = r.Next(1, 100);
+		}
+
+		private void btnPrüfen_Click(object sender, EventArgs e)
+		{
+			// Eingabe aus dem Textfeld auslesen, konvertieren und in einer
+			// Variable eines passenden Datentyps speichern
+			int eingabe = Convert.ToInt32(txtEingabe.Text);
+
+			// Prüfen, ob Zahl erraten wurde => Ausgabe Erfolgsmeldung
+			if (eingabe == zufallszahl) lblAusgabe.Text = "Zahl erraten!";
+
+			// Prüfen, ob Zahl kleiner als Zufallszahl ist => Hinweis ausgeben
+			if (eingabe < zufallszahl) lblAusgabe.Text = "Eingegebene Zahl " + eingabe + " ist zu klein!";
+
+			// Prüfen, ob Zahl größer als Zufallszahl ist => Hinweis ausgeben
+			if (eingabe > zufallszahl) lblAusgabe.Text = "Eingegebene Zahl " + eingabe + " ist zu groß!";
+		}
+	}
+}
